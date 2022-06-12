9d28ba6c085fc5af9bcfdbc5c1029b914dcab4ce
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-06-12 22:30:23 +0530

    Change chain

--- minter/pages/index.js
+++ minter/pages/index.js
@@ -2,14 +2,13 @@ import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import Button from "../src/components/Button";
 import Loader from "../src/components/Loader";
 
 import Form from "../src/components/Form";
 
 import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI } from "../src/constants";
-
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
@@ -24,29 +23,25 @@ export default function Home() {
   const web3ModalRef = useRef();
 
   const publicMint = async () => {
-    if (metadataURLIPFS !== "") {
-      try {
-        const signer = await getProviderOrSigner(true);
+    try {
+      const signer = await getProviderOrSigner(true);
 
-        const NFTeeContract = new Contract(
-          NFT_CONTRACT_ADDRESS,
-          NFT_CONTRACT_ABI,
-          signer
-        );
+      const NFTeeContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        signer
+      );
 
-        const tx = await NFTeeContract.mintNFT(metadataURLIPFS);
+      const tx = await NFTeeContract.mint({ value: utils.parseEther("0.1") });
 
-        setLoading(true);
-        await tx.wait();
+      setLoading(true);
+      await tx.wait();
 
-        setLoading(false);
-        setNftUrl(true);
-        toast.success("You successfully minted a Masterpiece!");
-      } catch (err) {
-        console.error(err);
-      }
-    } else {
-      toast.error("Please upload an image");
+      setLoading(false);
+      setNftUrl(true);
+      toast.success("You successfully minted a TeamTree NFT!");
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -78,9 +73,9 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      toast.error("Change the network to Rinkeby")
-      throw new Error("Change network to Rinkeby");
+    if (chainId !== 43113) {
+      toast.error("Change the network to Avalanche");
+      throw new Error("Change network to Avalanche");
     }
 
     if (needSigner) {
@@ -93,12 +88,11 @@ export default function Home() {
   useEffect(() => {
     if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
-        network: "rinkeby",
+        network: "AVAX",
         providerOptions: {},
         disableInjectedProvider: false,
       });
       connectWallet();
-
     }
   }, [walletConnected]);
 
@@ -107,36 +101,34 @@ export default function Home() {
       return <Button text={"Connect your wallet"} onClick={connectWallet} />;
     if (loading) return <Loader />;
     else {
-      return <Button onClick={publicMint} text="Mint 🚀" />;
+      return <Button onClick={publicMint} text='Mint 🚀' />;
     }
   };
 
   return (
-    <div className="m-2">
+    <div className='m-2'>
       <Head>
-        <title>Create NFTS</title>
-        <meta name="description" content="Create your moments as NFTs" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Team Tree</title>
+        <meta name='description' content='Create your moments as NFTs' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <Toaster />
 
       {nftUrl && (
-
-        <div className="text-center text-white m-4">
+        <div className='text-center text-white m-4'>
           <a
-            className="text-lg font-medium  text-white cursor-pointer underline"
-            href={`https://testnets.opensea.io/assets/${NFT_CONTRACT_ADDRESS}/${1}`}
-          >
+            className='text-lg font-medium  text-white cursor-pointer underline'
+            href={`https://testnets.opensea.io/assets/${NFT_CONTRACT_ADDRESS}/${1}`}>
             Here is your NFT ➡️
           </a>
         </div>
       )}
 
-      <p className="text-white text-center text-2xl pt-2">Make NFTs Like Pros</p>
+      <p className='text-white text-center text-2xl pt-2'>
+        Make NFTs Like Pros
+      </p>
 
-      <Form setMetadataURLIPFS={setMetadataURLIPFS} />
-
-      <div className="max-w-sm mx-auto text-center mt-8 text-xl font-medium ">
+      <div className='max-w-sm mx-auto text-center mt-8 text-xl font-medium '>
         {renderButton()}
       </div>
     </div>
--- minter/src/components/NavigationBar.js
+++ minter/src/components/NavigationBar.js
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 export default function NavigationBar() {
   return (
-    <nav className="rounded-lg p-4 mx-auto max-w-4xl mt-4 overflow-x-auto bg-white text-black ">
-      <div className="flex space-x-4 ">
-        <Link href="/">
-          <a className="text-lg font-medium p-1 hover:bg-slate-400 hover:rounded ease-in">
+    <nav className='rounded-lg p-4 mx-auto max-w-4xl mt-4 overflow-x-auto bg-white text-black '>
+      <div className='flex space-x-4 '>
+        <Link href='/'>
+          <a className='text-lg font-medium p-1 hover:bg-slate-400 hover:rounded ease-in'>
             Home
           </a>
         </Link>
 
-        <Link href="/about">
-          <a className="text-lg font-medium p-1 hover:bg-slate-400 hover:rounded">
+        <Link href='/about'>
+          <a className='text-lg font-medium p-1 hover:bg-slate-400 hover:rounded'>
             About
           </a>
         </Link>
 
-        <Link href="https://twitter.com/itsanishjain">
-          <a className="text-lg font-medium p-1 hover:bg-slate-400 hover:rounded">
+        <Link href='https://twitter.com/ayushgupta0010'>
+          <a className='text-lg font-medium p-1 hover:bg-slate-400 hover:rounded'>
             Contact
           </a>
         </Link>
